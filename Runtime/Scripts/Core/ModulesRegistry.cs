31e269a6e2b6e716c3bc7e0a794ff7146aa18358
Bronius Barsauskas <bronius@estotyvilnius.com>
2021-11-27 13:57:11 +0200

    Comment update

--- Runtime/Scripts/Core/ModulesRegistry.cs
+++ Runtime/Scripts/Core/ModulesRegistry.cs
@@ -5,8 +5,8 @@ namespace DosinisSDK.Core
     public class ModulesRegistry : MonoBehaviour
     {
         /// <summary>
-        /// Add desired non-behaviour modules in override of this method.
-        /// You may also pass Module config argument in app.RegisterModule(new Module(), moduleConfig);
+        /// Add desired modules in override of this method.
+        /// NOTE: You may also pass Module config argument in app.RegisterModule(new Module(), moduleConfig);
         /// </summary>
         /// <param name="app"></param>
         public virtual void Init(IApp app)
