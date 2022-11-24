38ee642da7d1eb5d4af5d932349bd0076cd1a3fc
dosinis <b.barsauskas1@gmail.com>
2022-11-24 09:38:28 +0200

    Moved SimpleInput to Tools

--- Runtime/Scripts/SimpleInput/InputHandler.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace DosinisSDK.SimpleInput
-{
-    public abstract class InputHandler : MonoBehaviour
-    {
-        public Vector2 Direction { get; protected set; }
-    }
-}
