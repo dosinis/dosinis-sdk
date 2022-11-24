0db064f173dd80f4f09936cd3a76bd2089623b9c
dosinis <b.barsauskas1@gmail.com>
2022-11-24 15:24:29 +0200

    Adde Helper util

---
+++ Runtime/Scripts/Utils/Helper.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+namespace DosinisSDK.Utils
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Random roll chance. Input is between 0 and 1 or 0 and 100 (if useHundreds is true)
+        /// </summary>
+        /// <returns></returns>
+        public static bool RollChance(float chance, bool useHundreds = false)
+        {
+            if (chance == 0)
+                return false;
+
+            if (useHundreds)
+            {
+                return Random.Range(0f, 100f) <= chance;
+            }
+            return Random.Range(0f, 1f) <= chance;
+        }
+
+        /// <summary>
+        /// Random roll chance. Input is between 0 and 100
+        /// </summary>
+        /// <returns></returns>
+        public static bool RollChance(int chance)
+        {
+            return Random.Range(1, 101) <= chance;
+        }
+        
+        /// <summary>
+        /// Returns the same angle from eulerAngles, that is displayed in the inspector
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        public static float TranslateEulerAngles(float angle)
+        {
+            angle %= 360;
+            
+            if (angle > 180)
+                return angle - 360;
+
+            return angle;
+        }
+        
+        /// <summary>
+        /// Returns DateTime constructed from now and X seconds delay
+        /// </summary>
+        /// <param name="seconds"></param>
+        /// <returns></returns>
+        public static DateTime GetDateTimeAfterSeconds(long seconds)
+        {
+            long time = DateTimeOffset.Now.ToUnixTimeSeconds() + seconds;
+
+            return DateTimeOffset.FromUnixTimeSeconds(time).DateTime.ToLocalTime();
+        }
+
+        /// <summary>
+        /// Converts Dictionary to json. Useful for analytics data.
+        /// NOTE: collection nesting support is up to one layer only.
+        /// </summary>
+        /// <param name="dictionary"></param>
+        /// <returns></returns>
+        public static string ConvertDictionaryToJson(Dictionary<string, object> dictionary)
+        {
+            string json = $"{{";
+
+            int size = dictionary.Count;
+            int i = 0;
+
+            foreach (var pair in dictionary)
+            {
+                if (pair.Value is IEnumerable && pair.Value is string == false)
+                {
+                    string collectionJson = "";
+
+                    var collection = (IEnumerable)pair.Value;
+
+                    var colSize = collection.Count();
+                    int x = 0;
+
+                    foreach (var element in collection)
+                    {
+                        if (colSize <= x + 1)
+                        {
+                            collectionJson += $"{element}";
+                        }
+                        else
+                        {
+                            collectionJson += $"{element},";
+                        }
+
+                        x++;
+                    }
+
+                    if (size <= i + 1)
+                    {
+                        json += $"\"{pair.Key}\": \"{collectionJson}\"";
+                    }
+                    else
+                    {
+                        json += $"\"{pair.Key}\": \"{collectionJson}\", ";
+                    }
+                }
+                else if (double.TryParse(pair.Value.ToString(), out double result))
+                {
+                    if (size <= i + 1)
+                    {
+                        json += $"\"{pair.Key}\": {result}";
+                    }
+                    else
+                    {
+                        json += $"\"{pair.Key}\": {result}, ";
+                    }
+                }
+                else
+                {
+                    if (size <= i + 1)
+                    {
+                        json += $"\"{pair.Key}\": \"{pair.Value}\"";
+                    }
+                    else
+                    {
+                        json += $"\"{pair.Key}\": \"{pair.Value}\", ";
+                    }
+                }
+
+                i++;
+            }
+
+            json += $"}}";
+
+            return json;
+        }
+    }
+}
