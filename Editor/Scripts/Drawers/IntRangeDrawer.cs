d431155a17c43ec5f23c9dc98e1ae278bbeaac4f
dosinis <b.barsauskas1@gmail.com>
2022-11-24 09:39:10 +0200

    Added BigNumber and some property drawers

---
+++ Editor/Scripts/Drawers/IntRangeDrawer.cs
@@ -0,0 +1,28 @@
+using DosinisSDK.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace DosinisSDK.Editor
+{
+    [CustomPropertyDrawer(typeof(IntRange))]
+    public class IntRangeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+			
+            EditorGUI.indentLevel++;
+			
+            var levelRect = new Rect(position.x - 15, position.y, 100, position.height);
+            var valueRect = new Rect(position.x + 75, position.y, 100, position.height);
+			
+            EditorGUI.PropertyField(levelRect, property.FindPropertyRelative("min"), GUIContent.none);
+            EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("max"), GUIContent.none);
+            
+            EditorGUI.indentLevel--;
+			
+            EditorGUI.EndProperty();
+        }
+    }
+}
