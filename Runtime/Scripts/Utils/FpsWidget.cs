a1fec51d8ce8a8c2444d5422cc3250fd51e0d374
dosinis <b.barsauskas1@gmail.com>
2023-01-23 11:11:24 +0200

    Refactored fps widget

--- Runtime/Scripts/Utils/FpsWidget.cs
+++ Runtime/Scripts/Utils/FpsWidget.cs
@@ -3,24 +3,20 @@ using UnityEngine;
 
 namespace DosinisSDK.Utils
 {
+    [RequireComponent(typeof(TMP_Text))]
     public class FpsWidget : MonoBehaviour
     {
-        private TMP_Text text;
+        [SerializeField] private TMP_Text text;
 
-        private float hudRefreshRate = 1;
+        private const float HUD_REFRESH_RATE = 1;
         private float timer;
 
-        private void Awake()
+#if UNITY_EDITOR
+        private void Reset()
         {
-            if (gameObject.TryGetComponent(out TMP_Text txt))
-            {
-                text = txt;
-            }
-            else
-            {
-                text = gameObject.AddComponent<TMP_Text>();
-            }
+            text = GetComponent<TMP_Text>();
         }
+#endif
 
         private void Update()
         {
@@ -28,7 +24,7 @@ namespace DosinisSDK.Utils
             {
                 int fps = (int)(1f / Time.unscaledDeltaTime);
                 text.text = "FPS: " + fps;
-                timer = Time.unscaledTime + hudRefreshRate;
+                timer = Time.unscaledTime + HUD_REFRESH_RATE;
             }
         }
     }
