7bc02150112c371bc28758077b8154e561ea7a09
dosinis <b.barsauskas1@gmail.com>
2024-02-15 11:37:39 +0200

    Added EditorUtils script

---
+++ Runtime/Scripts/Utils/EditorUtils.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace DosinisSDK.Utils
+{
+    public class EditorUtils
+    {
+        /// <summary>
+        /// Get all assets of type T
+        /// </summary>
+        /// <typeparam name="T">type of the asset</typeparam>
+        /// <returns></returns>
+        public static T[] GetAssetsOfType<T>() where T : Object
+        {
+            var assets = AssetDatabase.FindAssets($"t:{typeof(T).Name}")
+                .Select(guid => AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid))).ToArray();
+
+            return assets;
+        }
+        
+        /// <summary>
+        /// Set array of assets of type T to the array and save the changes
+        /// </summary>
+        /// <param name="array">ref array</param>
+        /// <param name="targetObj">target object that will be set dirty</param>
+        public static void GetAssetsOfType<T>(ref T[] array, Object targetObj) where T : Object
+        {
+            array = GetAssetsOfType<T>();
+            
+            EditorUtility.SetDirty(targetObj);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
+#endif
