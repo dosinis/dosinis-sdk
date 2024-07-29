c0b6085cdcad2bdd041393987c40230b22ccf668
dosinis <b.barsauskas1@gmail.com>
2024-07-29 10:10:15 +0300

    Fixed Attribute drawers failing on 2022 unity

--- Editor/Scripts/EditorHelper.cs
+++ Editor/Scripts/EditorHelper.cs
@@ -13,23 +13,28 @@ namespace DosinisSDK.Editor
             var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
             var scriptAttributeUtility = assembly.CreateInstance("UnityEditor.ScriptAttributeUtility");
             var scriptAttributeUtilityType = scriptAttributeUtility.GetType();
- 
+
             var bindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
             var getDrawerTypeForType = scriptAttributeUtilityType.GetMethod("GetDrawerTypeForType", bindingFlags);
- 
-            return (Type)getDrawerTypeForType.Invoke(scriptAttributeUtility, new object[] { classType });
+
+#if UNITY_2022_1_OR_NEWER
+            var type = getDrawerTypeForType.Invoke(scriptAttributeUtility, new object[] { classType, null });
+#else
+            var type = getDrawerTypeForType.Invoke(scriptAttributeUtility, new object[] { classType });
+#endif
+            return type as Type;
         }
 
         public static bool DrawCustomProperty(SerializedProperty property, Rect position, ref Rect offsetPosition)
         {
             var parentType = property.serializedObject.targetObject.GetType();
-            
+
             var field = Helper.GetFieldInfoWithReflection(property.propertyPath, parentType);
-            
+
             if (field != null)
             {
                 var fieldType = field.FieldType;
-                    
+
                 var drawerType = GetPropertyDrawerType(fieldType);
 
                 if (drawerType == null)
@@ -37,7 +42,7 @@ namespace DosinisSDK.Editor
                     Debug.LogWarning($"Cannot find drawer for: {fieldType} type. Using default drawer");
                     return false;
                 }
-                
+
                 var guiMethod = drawerType.GetMethod("OnGUI", BindingFlags.Instance | BindingFlags.Public);
 
                 if (guiMethod == null)
@@ -47,14 +52,16 @@ namespace DosinisSDK.Editor
                 }
 
                 var drawer = (PropertyDrawer)Activator.CreateInstance(drawerType);
-                
+
                 offsetPosition.size = new Vector2(offsetPosition.size.x, offsetPosition.size.y);
-                
-                guiMethod.Invoke(drawer, new object[] { offsetPosition, property, new GUIContent(property.displayName) });
+
+                guiMethod.Invoke(drawer,
+                    new object[] { offsetPosition, property, new GUIContent(property.displayName) });
                 return true;
             }
 
-            Debug.LogWarning("Cannot find field: " + property.propertyPath + " in " + parentType + " type. Using default drawer");
+            Debug.LogWarning("Cannot find field: " + property.propertyPath + " in " + parentType +
+                             " type. Using default drawer");
             return false;
         }
     }
