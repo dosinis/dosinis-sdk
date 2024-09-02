ae80c2a122d750149284a6cdca7c67cb2454f172
dosinis <b.barsauskas1@gmail.com>
2024-09-02 10:03:50 +0300

    Setting root dirty after moving to root

--- Editor/Scripts/HierarchyNavManager.cs
+++ Editor/Scripts/HierarchyNavManager.cs
@@ -1,4 +1,6 @@
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace DosinisSDK.Editor
 {
@@ -7,11 +9,24 @@ namespace DosinisSDK.Editor
         [MenuItem("Assets/Dosinis/MoveToRoot %g")]
         private static void MoveToRoot()
         {
+            if (PrefabStageUtility.GetCurrentPrefabStage() != null)
+            {
+                Debug.LogWarning("Can't move to root in prefab context mode!");
+                return;
+            }
+            
+            Transform root = null;
+            
             foreach (var s in Selection.transforms)
             {
-                var root = s.root;
+                root = s.root;
                 s.SetParent(root);
                 s.SetAsFirstSibling();
+            }
+
+            if (root)
+            {
+                EditorUtility.SetDirty(root);
             }
         }
     }
