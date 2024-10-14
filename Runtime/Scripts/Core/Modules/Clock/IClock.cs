334a014b5601f6117b93dc26bc4721f70cdfcb45
LukasKryzauskas <33089108+Snitchel@users.noreply.github.com>
2024-10-14 10:03:58 +0300

    Add lastActiveDay value (#4)
    Co-authored-by: LukasK <lukosius181@gmial.com>

--- Runtime/Scripts/Core/Modules/Clock/IClock.cs
+++ Runtime/Scripts/Core/Modules/Clock/IClock.cs
@@ -3,6 +3,7 @@ namespace DosinisSDK.Core
     public interface IClock : IModule 
     {
         public int DayOfYear { get; }
+        public int LastActiveDayOfYear { get; }
         public long LastTimeActive { get; }
         public long Now { get; }
         public long NowMilliseconds { get; }
--- Runtime/Scripts/Core/Modules/Clock/LocalClockData.cs
+++ Runtime/Scripts/Core/Modules/Clock/LocalClockData.cs
@@ -3,5 +3,6 @@ namespace DosinisSDK.Core
     public class LocalClockData : ModuleData
     {
         public long lastTimeActive;
+        public int lastActiveDayOfYear;
     }
 }
