a61d2b0a4e1b329822a23a8184c617720f548d45
Nicel193 <64494378+Nicel193@users.noreply.github.com>
2025-06-29 02:13:51 +0300

    Updated FocusAround

--- Runtime/Scripts/UI/Elements/InfiniteElementSet.cs
+++ Runtime/Scripts/UI/Elements/InfiniteElementSet.cs
@@ -12,14 +12,14 @@ namespace DosinisSDK.UI.Elements
         [SerializeField] private RectTransform viewport = null;
         [SerializeField] private float anchorOffset = 0;
         [SerializeField] private bool isVertical = true;
-        
+
         private readonly List<object> valuesCache = new();
 
         private float elementSize;
         private int currentPivot;
         private int visibleElementCount;
         private RectTransform anchor = null;
-        
+
         private Element[] currentElements = Array.Empty<Element>();
         private Element[] elementsCache = Array.Empty<Element>();
 
@@ -35,19 +35,19 @@ namespace DosinisSDK.UI.Elements
                 return;
             }
 
-            float currentPosition = isVertical ? anchor.anchoredPosition.y : anchor.anchoredPosition.x;
-            int pivot = Mathf.Clamp(Mathf.CeilToInt((Mathf.Abs(currentPosition) + anchorOffset - elementSize) / elementSize),
+            float currentPosition = isVertical ? anchor.anchoredPosition.y : Mathf.Abs(anchor.anchoredPosition.x);
+            int pivot = Mathf.Clamp(Mathf.CeilToInt((currentPosition + anchorOffset - elementSize) / elementSize),
                 0, valuesCache.Count - visibleElementCount);
-            
+
             while (pivot > currentPivot)
             {
                 if (pivot + visibleElementCount - 1 >= valuesCache.Count)
                 {
                     currentPivot = pivot;
-                    
+
                     break;
                 }
-                
+
                 currentPivot += (int)Mathf.Sign(pivot - currentPivot);
 
                 var current = currentElements;
@@ -67,11 +67,11 @@ namespace DosinisSDK.UI.Elements
                 elementsCache = current;
 
                 float elementToSwapPosition = isVertical
-                    ? elementToSwap.RectTransform.anchoredPosition.y
+                    ? -elementToSwap.RectTransform.anchoredPosition.y
                     : elementToSwap.RectTransform.anchoredPosition.x;
-                
+
                 float finalPosition = elementToSwapPosition + elementSize * visibleElementCount;
-                
+
                 elementToSwap.RectTransform.anchoredPosition = isVertical
                     ? new Vector2(elementToSwap.RectTransform.anchoredPosition.x, finalPosition)
                     : new Vector2(finalPosition, elementToSwap.RectTransform.anchoredPosition.y);
@@ -82,7 +82,7 @@ namespace DosinisSDK.UI.Elements
                 if (pivot < 0)
                 {
                     currentPivot = 0;
-                    
+
                     break;
                 }
 
@@ -103,35 +103,37 @@ namespace DosinisSDK.UI.Elements
                 currentElements = elementsCache;
 
                 elementsCache = current;
-                
+
                 float elementToSwapPosition = isVertical
-                    ? elementToSwap.RectTransform.anchoredPosition.y
+                    ? -elementToSwap.RectTransform.anchoredPosition.y
                     : elementToSwap.RectTransform.anchoredPosition.x;
-                
+
                 float finalPosition = elementToSwapPosition - elementSize * visibleElementCount;
-                
+
                 elementToSwap.RectTransform.anchoredPosition = isVertical
                     ? new Vector2(elementToSwap.RectTransform.anchoredPosition.x, finalPosition)
                     : new Vector2(finalPosition, elementToSwap.RectTransform.anchoredPosition.y);
             }
         }
-        
-        public void FocusAround<T>(T value)
+
+        public Element FocusAround<T>(T value)
         {
             int index = valuesCache.IndexOf(value);
+            int elementIndex = index - (Mathf.FloorToInt(index / (float)currentElements.Length) * currentElements.Length); 
             
             if (index < 0)
             {
-                return;
+                return null;
             }
-            
-            float offset = elementSize * -index;
-            
-            anchor.anchoredPosition = isVertical
-                ? new Vector2(anchor.anchoredPosition.x, offset)
-                : new Vector2(offset, anchor.anchoredPosition.y);
-        }
 
+            anchor.anchoredPosition = isVertical
+                ? new Vector2(anchor.anchoredPosition.x, (elementSize * index + elementSize
+                    / 2) - visibleElementCount / 2f * elementSize)
+                : new Vector2(elementSize * -index, anchor.anchoredPosition.y);
+            
+            return GetElement(elementIndex);
+        }
+        
         public override void Setup<TE, T>(IEnumerable<T> objects)
         {
             if (anchor == null)
@@ -140,18 +142,20 @@ namespace DosinisSDK.UI.Elements
             }
 
             float viewSize = isVertical ? viewport.rect.height : viewport.rect.width;
-            
-            elementSize = isVertical ? element.RectTransform.rect.height + spacing : element.RectTransform.rect.width + spacing;
+
+            elementSize = isVertical
+                ? element.RectTransform.rect.height + spacing
+                : element.RectTransform.rect.width + spacing;
             visibleElementCount = Mathf.CeilToInt(viewSize / elementSize) + 1;
-            
+
             var enumerable = objects as T[] ?? objects.ToArray();
-            
+
             float contentSize = elementSize * enumerable.Length;
             if (isVertical)
                 anchor.SetHeight(contentSize);
             else
                 anchor.SetWidth(contentSize);
-            
+
             currentPivot = 0;
             valuesCache.Clear();
             Clear();
@@ -161,7 +165,7 @@ namespace DosinisSDK.UI.Elements
                 currentElements = new Element[visibleElementCount];
                 elementsCache = new Element[visibleElementCount];
             }
-            
+
             element.Hide();
 
             int i = 0;
@@ -180,19 +184,21 @@ namespace DosinisSDK.UI.Elements
                         e.Init();
                         spawnedElements.Add(e);
                     }
+
                     currentElements[i] = e;
 
                     if (e is ElementFor<T> ef)
                     {
                         ef.Setup(value);
                     }
-                    
+
                     e.Show();
-                    
+
                     e.RectTransform.anchoredPosition = isVertical
-                        ? new Vector2(e.RectTransform.anchoredPosition.x, i * elementSize)
+                        ? new Vector2(e.RectTransform.anchoredPosition.x, i * -elementSize)
                         : new Vector2(i * elementSize, e.RectTransform.anchoredPosition.y);
                 }
+
                 valuesCache.Add(value);
                 i++;
             }
